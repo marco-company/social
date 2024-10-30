d14b2753c7add16e7c3f6255cbc0aa2ce5049c36
hugues de keyzer <odoo@hugues.info>
2024-10-30 09:29:54 +0100

    [MIG] mail_activity_filter_internal_user: migration to 16.0

--- mail_activity_filter_internal_user/models/mail_activity.py
+++ mail_activity_filter_internal_user/models/mail_activity.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Coop IT Easy SC
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class MailActivity(models.Model):
@@ -12,15 +12,3 @@ class MailActivity(models.Model):
         string="Assign to internal user only",
         default=True,
     )
-
-    @api.onchange("filter_internal_user")
-    def onchange_filter_internal_user(self):
-        """
-        Change the domain on the user_id field based on the value of
-        filter_internal_user.
-        """
-        # copy to ensure original domain is not modified
-        user_domain = self._fields["user_id"].domain.copy()
-        if self.filter_internal_user:
-            user_domain.append(("share", "=", False))
-        return {"domain": {"user_id": user_domain}}
